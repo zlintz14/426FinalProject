d4f38f78ae65687e446a299347e3e1b11bb7c2c3
DanielWeber97 <daniel.weber.443@gmail.com>
2019-12-02 18:35:26 -0500

    changed post to get

--- workout-buddy/src/components/User.js
+++ workout-buddy/src/components/User.js
@@ -2,30 +2,34 @@ import React from "react";
 
 let myStorage = window.localStorage;
 let jwt = myStorage.getItem("jwt");
-console.log(jwt)
 const axios = require("axios");
 let userInfo;
 let header = `Bearer ${jwt}`;
-console.log(header);
-axios
-  .post("http://localhost:3000/account/status", {
-    headers: {
-        Authorization: 'Bearer ' + jwt //the token is a variable which holds the token
+
+let getUserInfo = async function() {
+  await axios
+    .get("http://localhost:3000/account/status", {
+      headers: {
+        Authorization: "Bearer " + jwt //the token is a variable which holds the token
       }
-  })
-  .then(response => {
-    // userInfo = response;
-      console.log('logging res')
-    console.log(response)
-  })
-  .catch(e => {
-      console.log('logging error')
-    console.log(e);
-  });
+    })
+    .then(response => {
+      userInfo = response;
+    })
+    .catch(e => {
+      console.log(e);
+    });
+
+  return (
+    <div>
+      <p>Hello{userInfo.data.user.data}!</p>
+    </div>
+  );
+};
 function User(props) {
   return (
     <div>
-      <p>Hello!</p>
+      <p>Hello User!</p>
     </div>
   );
 }
